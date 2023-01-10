61ca220bb379ca0a855d2be2d982c31a2af96945
Anshika Gusain <anshikagusain62@gmail.com>
2023-01-10 17:53:49 +0530

    leveling up to bcrypt

--- app.js
+++ app.js
@@ -2,8 +2,10 @@ require("dotenv").config();
 const express=require("express");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
-const encrypt=require("mongoose-encryption");
-const md5=require("md5");
+// const encrypt=require("mongoose-encryption");
+// const md5=require("md5");
+const bcrypt=require("bcrypt");
+const saltRounds=10;
 const app=express();
 app.set("view engine","ejs");
 app.use(express());
@@ -34,30 +36,40 @@ app.get("/register",(req,res)=>{
 })
 
 app.post("/register",(req,res)=>{
-    const user=new User({
-        email:req.body.username,
-        password:md5(req.body.password)
-    })
-    user.save((err)=>{
-        if(err){
-            console.log(err);
-        }
-        else{
-            res.render("secrets");
-        }
-    })
+    bcrypt.hash(req.body.password,saltRounds,(err,hash)=>{
+        const user=new User({
+            email:req.body.username,
+            password:hash
+        })
+        user.save((err)=>{
+            if(err){
+                console.log(err);
+            }
+            else{
+                res.render("secrets");
+            }
+        })
+    });
 })
 
 app.post("/login",(req,res)=>{
     const username=req.body.username;
-    const password=md5(req.body.password);
+    // const password=md5(req.body.password);
+    const password=req.body.password;
     User.findOne({email:username},(err,user)=>{
-        if(err)res.send(err);
+        if(err){
+            res.send(err);
+        }
         else{
             if(user){
-                if(user.password===password){
-                    res.render("secrets");
-                }
+                bcrypt.compare(password, user.password, function(err, result) {
+                    if(result===true){
+                        res.render("secrets");
+                    }
+                });
+                // if(user.password===password){
+                //     res.render("secrets");
+                // }
             }
         }
     })
